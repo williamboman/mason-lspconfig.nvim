beec34cb3218aca5ae8e3e81a8c35d47fb0848e3
William Boman <william@redwill.se>
2023-01-06 11:42:54 +0100

    feat(solang): expand llvm path instead of hard-coding it

--- lua/mason-lspconfig/server_configurations/solang/init.lua
+++ lua/mason-lspconfig/server_configurations/solang/init.lua
@@ -6,8 +6,7 @@ return function(install_dir)
     return {
         cmd_env = {
             PATH = process.extend_path {
-                path.concat { install_dir, "llvm13.0", "bin" },
-                path.concat { install_dir, "llvm12.0", "bin" }, -- kept for backwards compatibility
+                vim.fn.expand(path.concat { install_dir, "llvm*", "bin" }, true, true)[1],
             },
         },
     }
