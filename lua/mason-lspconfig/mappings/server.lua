d7ff61a828d59bc593ea3e2020508c114048d790
Cason Adams <casonadams@gmail.com>
2023-01-22 13:23:03 -0700

    feat: add bright_script (#133)

--- lua/mason-lspconfig/mappings/server.lua
+++ lua/mason-lspconfig/mappings/server.lua
@@ -15,6 +15,7 @@ M.lspconfig_to_package = {
     ["bashls"] = "bash-language-server",
     ["beancount"] = "beancount-language-server",
     ["bicep"] = "bicep-lsp",
+    ["bright_script"] = "brighterscript",
     ["bsl_ls"] = "bsl-language-server",
     ["bufls"] = "buf-language-server",
     ["clangd"] = "clangd",
