1749f38d0bf754a6d4410abe1ac3498f70d98f0e
William Boman <william@redwill.se>
2023-03-02 02:23:25 +0100

    feat(solang): also include $MASON/opt/solang llvm bins
    This will be the new location of LLVM-patched bins once https://github.com/mason-org/mason-registry/ is live.

--- lua/mason-lspconfig/server_configurations/solang/init.lua
+++ lua/mason-lspconfig/server_configurations/solang/init.lua
@@ -3,11 +3,13 @@ local process = require "mason-core.process"
 
 ---@param install_dir string
 return function(install_dir)
+    local llvm_bins = { vim.fn.expand(path.concat { install_dir, "llvm*", "bin" }, true, true)[1] }
+    if vim.env.MASON then
+        table.insert(llvm_bins, vim.fn.expand "$MASON/opt/solang/llvm15.0/bin")
+    end
     return {
         cmd_env = {
-            PATH = process.extend_path {
-                vim.fn.expand(path.concat { install_dir, "llvm*", "bin" }, true, true)[1],
-            },
+            PATH = process.extend_path(llvm_bins),
         },
     }
 end
