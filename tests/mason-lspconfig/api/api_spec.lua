341c56587002bf6ca9fab0d0b60fd2006fed98be
William Boman <william@redwill.se>
2023-03-24 11:20:14 +0100

    fix(scripts): setup mason and refresh registry
    Also fix tests.

--- tests/mason-lspconfig/api/api_spec.lua
+++ tests/mason-lspconfig/api/api_spec.lua
@@ -3,30 +3,39 @@ local mason_lspconfig = require "mason-lspconfig"
 
 describe("mason-lspconfig API", function()
     it("should return all available servers", function()
-        local server_mappings = require "mason-lspconfig.mappings.server"
         local available_servers = mason_lspconfig.get_available_servers()
-        assert.equals(#vim.tbl_keys(server_mappings.package_to_lspconfig), #available_servers)
+        assert.equals(vim.tbl_count(require "dummy-registry.index"), #available_servers)
     end)
 
     it("should return all available servers for given filetype", function()
+        require("mason-lspconfig.mappings.filetype").dummylang = {
+            "dummylsp",
+            "dummy2lsp",
+        }
         assert.same(
-            { "terraformls", "tflint" },
+            { "dummy2lsp", "dummylsp" },
             _.sort_by(
                 _.identity,
                 mason_lspconfig.get_available_servers {
-                    filetype = "terraform",
+                    filetype = "dummylang",
                 }
             )
         )
     end)
 
     it("should return all available servers for given filetypes", function()
+        require("mason-lspconfig.mappings.filetype").dummylang = {
+            "dummylsp",
+        }
+        require("mason-lspconfig.mappings.filetype").madeuplang = {
+            "dummy2lsp",
+        }
         assert.same(
-            { "lemminx", "taplo" },
+            { "dummy2lsp", "dummylsp" },
             _.sort_by(
                 _.identity,
                 mason_lspconfig.get_available_servers {
-                    filetype = { "xml", "xsd", "xsl", "toml" },
+                    filetype = { "dummylang", "madeuplang" },
                 }
             )
         )
