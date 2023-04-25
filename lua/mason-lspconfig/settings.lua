5b221c253212e1cc400bffd75eb0029b78d43642
William Boman <william@redwill.se>
2023-04-25 06:02:07 +0200

    chore: add more type annotations to settings tbl

--- lua/mason-lspconfig/settings.lua
+++ lua/mason-lspconfig/settings.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 ---@class MasonLspconfigSettings
----@field handlers table<string, fun(server_name: string)> | nil
 local DEFAULT_SETTINGS = {
     -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
     -- This setting has no relation with the `automatic_installation` setting.
+    ---@type string[]
     ensure_installed = {},
+
     -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
     -- This setting has no relation with the `ensure_installed` setting.
     -- Can either be:
@@ -13,8 +14,11 @@ local DEFAULT_SETTINGS = {
     --   - true: All servers set up via lspconfig are automatically installed.
     --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
     --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    ---@type boolean
     automatic_installation = false,
-    --See `:h mason-lspconfig.setup_handlers()`
+
+    -- See `:h mason-lspconfig.setup_handlers()`
+    ---@type table<string, fun(server_name: string)>?
     handlers = nil,
 }
 
