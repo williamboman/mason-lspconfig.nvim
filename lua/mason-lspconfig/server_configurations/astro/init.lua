9cd929e72565ea4e36d016c177e37d15362469a5
William Boman <william@redwill.se>
2023-06-05 12:44:01 +0200

    backwards compat

--- lua/mason-lspconfig/server_configurations/astro/init.lua
+++ lua/mason-lspconfig/server_configurations/astro/init.lua
@@ -4,6 +4,7 @@ local typescript = require "mason-lspconfig.typescript"
 return function(install_dir)
     return {
         on_new_config = function(new_config, workspace_dir)
+            new_config.init_options.typescript.serverPath = typescript.resolve_tsserver(install_dir, workspace_dir)
             new_config.init_options.typescript.tsdk = typescript.resolve_tsdk(install_dir, workspace_dir)
         end,
     }
