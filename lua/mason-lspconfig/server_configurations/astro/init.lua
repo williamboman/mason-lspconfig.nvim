d83f31d6ea081bb3a9af60a49cbffedb6e5c3b6a
An Hoang <xuanan2001@pm.me>
2023-06-05 17:49:10 +0700

    fix(astro): provide new `typescript.tsdk` initialization option (#235)
    Co-authored-by: William Boman <william@redwill.se>

--- lua/mason-lspconfig/server_configurations/astro/init.lua
+++ lua/mason-lspconfig/server_configurations/astro/init.lua
@@ -5,6 +5,7 @@ return function(install_dir)
     return {
         on_new_config = function(new_config, workspace_dir)
             new_config.init_options.typescript.serverPath = typescript.resolve_tsserver(install_dir, workspace_dir)
+            new_config.init_options.typescript.tsdk = typescript.resolve_tsdk(install_dir, workspace_dir)
         end,
     }
 end
