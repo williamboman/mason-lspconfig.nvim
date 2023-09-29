fdda2fb2f68c1eed2c1387e086c92a0af86d23e0
William Boman <william@redwill.se>
2023-09-29 14:10:04 +0200

    fix(apex_ls): fix jar location

--- lua/mason-lspconfig/server_configurations/apex_ls/init.lua
+++ lua/mason-lspconfig/server_configurations/apex_ls/init.lua
@@ -1,8 +1,5 @@
-local path = require "mason-core.path"
-
----@param install_dir string
-return function(install_dir)
+return function()
     return {
-        apex_jar_path = path.concat { install_dir, "apex-jorje-lsp.jar" },
+        apex_jar_path = vim.fn.expand "$MASON/share/apex-language-server/apex-jorje-lsp.jar",
     }
 end
