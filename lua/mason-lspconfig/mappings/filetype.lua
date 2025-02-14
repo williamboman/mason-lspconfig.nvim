c09fc2236cbc70eb262412c71b97e224fa80cd61
William Botman <william+bot@redwill.se>
2025-02-14 10:03:43 +0000

    chore: update generated code

--- lua/mason-lspconfig/mappings/filetype.lua
+++ lua/mason-lspconfig/mappings/filetype.lua
@@ -164,7 +164,7 @@ return {
   purescript = { "purescriptls" },
   python = { "ast_grep", "basedpyright", "dprint", "harper_ls", "jedi_language_server", "pylsp", "pylyzer", "pyre", "pyright", "ruff", "snyk_ls", "sourcery" },
   ql = { "codeqlls" },
-  quarto = { "ltex" },
+  quarto = { "ltex", "r_language_server" },
   r = { "r_language_server" },
   raku = { "raku_navigator" },
   razor = { "htmx", "tailwindcss" },
