f995805bbfea692653bfedb9e42872107b84ef72
William Boman <william@redwill.se>
2025-02-19 14:52:29 +0100

    v2.0.0-rc.1

--- lua/mason-lspconfig/version.lua
+++ lua/mason-lspconfig/version.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-M.VERSION = "v1.32.0" -- x-release-please-version
-M.MAJOR_VERSION = 1 -- x-release-please-major
-M.MINOR_VERSION = 32 -- x-release-please-minor
+M.VERSION = "v2.0.0-rc.1" -- x-release-please-version
+M.MAJOR_VERSION = 2 -- x-release-please-major
+M.MINOR_VERSION = 0 -- x-release-please-minor
 M.PATCH_VERSION = 0 -- x-release-please-patch
 
 return M
