485c1ba80573282c6e77aca6641469b9555bfe6a
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-08-31 15:19:45 +0200

    fix: append PowerShellEditorServices to bundle_path

--- lua/mason-lspconfig/lsp/powershell_es.lua
+++ lua/mason-lspconfig/lsp/powershell_es.lua
@@ -1,3 +1,3 @@
 return {
-    bundle_path = vim.fn.expand "$MASON/packages/powershell-editor-services",
+    bundle_path = vim.fn.expand "$MASON/packages/powershell-editor-services/PowerShellEditorServices",
 }
